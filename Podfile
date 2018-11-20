20ea94d63639ee8c02fbac84abbc62dd6e9872e3
Piotr Szmyt <piotr.szmyt@meelogic.com>
2018-11-20 13:05:47 +0100

    Initial upload

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '12.0'
+
+target 'TravisTest' do
+use_frameworks!
+  pod 'SwiftLint'
+end
---
+++ TravisTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravisTest
+//
+//  Created by Szmyt Piotr on 20/11/2018.
+//  Copyright © 2018 Szmyt Piotr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
